ca4751bd2925c8fa8c992c9da0342ca2ba8ba379
profknight <ahknight@pipeline.sbcc.edu>
2018-09-05 20:40:39 -0700

    Turning in assignment

---
+++ include/threeintegersort.h
@@ -0,0 +1,13 @@
+//
+// Created by aknight on 7/30/18.
+//
+
+#ifndef THREEINTEGERSORT_THREEINTEGERSORT_H
+#define THREEINTEGERSORT_THREEINTEGERSORT_H
+
+namespace edu { namespace vcccd { namespace vc { namespace csv13 {
+
+    void sort (int32_t &i1, int32_t &i2, int32_t &i3);
+
+}}}}
+#endif //THREEINTEGERSORT_THREEINTEGERSORT_H
---
+++ src/sort.cc
@@ -0,0 +1,36 @@
+//
+// Created by aknight on 8/16/18.
+//
+
+//
+// Created by aknight on 7/30/18.
+//
+
+#include <cstdint>
+
+namespace {
+
+    void swap(int32_t &i1, int32_t &i2) {
+        int32_t hold = i1;
+        i1 = i2;
+        i2 = hold;
+    }
+
+}
+
+namespace edu { namespace vcccd { namespace vc { namespace csv13 {
+
+    void sort(int32_t &i1, int32_t &i2, int32_t &i3) {
+        if (i1 > i2) {
+            swap(i1, i2);
+        }
+        if (i1 > i3) {
+            swap(i1, i3);
+        }
+
+        if (i2 > i3) {
+            swap(i2, i3);
+        }
+    }
+
+}}}}
